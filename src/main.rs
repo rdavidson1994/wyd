d5f522400b7674c5ef7ebcb536b4b3e22f4a2d2e
Robert Davidson <rdavidson@accusoft.com>
2021-05-29 15:00:43 -0400

    Switch to derive-based clap 3.0.0-beta.2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use chrono::{DateTime, Duration, Local, Utc};
 
-use std::{fmt::Display, fs};
+use std::{fmt::Display, fs, time::Duration as StdDuration};
 
 extern crate clap;
-use clap::{crate_version, App, AppSettings, Arg, ArgMatches, ArgSettings, SubCommand};
+use clap::{crate_version, AppSettings, ArgSettings, Clap};
 
 use std::default::Default;
 
@@ -34,120 +34,90 @@ pub trait StringMatch: FnMut(&str) -> bool {}
 
 impl<T> StringMatch for T where T: FnMut(&str) -> bool {}
 
-fn word_args_to_string(args: &ArgMatches) -> String {
-    args.values_of("word")
-        .unwrap_or_default()
-        .collect::<Vec<_>>()
-        .join(" ")
-}
-
 fn substring_matcher(pattern: &str) -> impl Fn(&str) -> bool + '_ {
     move |s: &str| -> bool { s.contains(pattern) }
 }
 
-fn main() {
-    let matches = App::new("What You're Doing")
-        .version(crate_version!())
-        .settings(&[AppSettings::InferSubcommands])
-        .subcommand(
-            SubCommand::with_name("push")
-                .about("Adds a new task to the top of the stack.")
-                .arg(
-                    Arg::with_name("timebox")
-                        .help("Time until task sends reminder notifications. (e.g. 1h 30m)")
-                        .long("timebox")
-                        .short("t")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("word").multiple(true).help(
-                        "Name of the new task. Supports bare words like `wyd push Send emails`",
-                    ),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("done").about("Marks the top task of the stack as complete"),
-        )
-        .subcommand({
-            let about = "Output reminders for expired timers".to_owned();
-            let about_extra = r#"
+#[derive(Clap, Debug)]
+//     let matches = App::new("What You're Doing")
+//         .version(crate_version!())
+//         .settings(&[AppSettings::InferSubcommands])
+#[clap(name = "What You're Doing")]
+#[clap(version = crate_version!())]
+#[clap(setting = AppSettings::InferSubcommands)]
 
-If you are using the notifier (created with `wyd spawn-notifier`), you
-shouldn't need this subcommand very much - the notifer effectively runs
-it every second. You may still occassionally find the `--force` flag useful,
-since it re-triggers reminders that have already sent notifiactions recently.
-"#;
-            SubCommand::with_name("remind")
-                .about(about.clone().as_str())
-                .long_about((about + about_extra).as_str())
-                .arg(
-                    Arg::with_name("force")
-                        .long("force")
-                        .short("f")
-                        .takes_value(false)
-                        .help("Re-send all active reminders, even recently sent ones."),
-                )
-        })
-        .subcommand(
-            SubCommand::with_name("ls")
-                .about("Prints a list of all tasks, including suspended ones."),
-        )
-        .subcommand(
-            SubCommand::with_name("notifier")
-                .about("Starts the notifier process, which sends wyd's reminder notifications.")
-                .arg(
-                    Arg::with_name("kill")
-                        .long("kill")
-                        .short("k")
-                        .takes_value(false)
-                        .help("Kills any active notifier processes."),
-                )
-                .arg(
-                    // Causes the active process to become the notifer.
-                    Arg::with_name("become")
-                        .long("become")
-                        .takes_value(true)
-                        .set(ArgSettings::Hidden),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("resume")
-                .arg(Arg::with_name("word").multiple(true))
-                .about("Resumes a suspended task"),
-        )
-        .subcommand(
-            SubCommand::with_name("suspend")
-                .about("Moves a task from the active stack to the suspended queue.")
-                .arg(
-                    Arg::with_name("reason")
-                        .long("reason")
-                        .short("r")
-                        .takes_value(true)
-                        .help("An optional note about why you suspended the task."),
-                )
-                .arg(
-                    Arg::with_name("timer")
-                        .long("timer")
-                        .short("t")
-                        .takes_value(true)
-                        .help("Sets a timer, after which the suspended task will send reminders."),
-                )
-                .arg(
-                    Arg::with_name("new")
-                        .long("new")
-                        .short("n")
-                        .takes_value(false)
-                        .help(
-                            "Creates a new suspended task instead of suspending an existing one.",
-                        ),
-                )
-                .arg(
-                    Arg::with_name("word")
-                        .multiple(true)
-                        .help("The name (or part of the name) of the task to be suspended."),
-                ),
-        )
-        .get_matches();
+enum Command {
+    /// Add a new task to the top of the stack.
+    Push {
+        /// Time until task sends reminder notifications. (e.g. 1h 30m)
+        #[clap(long, short)]
+        #[clap(parse(try_from_str = humantime::parse_duration))]
+        timebox: Option<StdDuration>,
+
+        /// Name of the new task. Supports bare words like `wyd push Send emails`
+        words: Vec<String>
+    },
+
+    /// Moves a task from the active stack to the suspended queue.
+    Suspend {
+        /// Sets a timer, after which the suspended task will send reminders.
+        #[clap(long, short)]
+        #[clap(parse(try_from_str = humantime::parse_duration))]
+        timebox: Option<StdDuration>,
+
+        /// Creates a new suspended task instead of suspending an existing one.
+        #[clap(long, short)]
+        new: bool,
+
+        /// An optional note about why you suspended the task.
+        #[clap(long, short, default_value = "None")]
+        reason: String,
+
+        /// The name (or part of the name) of the task to be suspended.
+        words: Vec<String>,
+
+    },
+
+    /// Marks the top task of the stack as complete
+    Done,
+
+    /// Output reminders for expired timers
+    Remind {
+        /// Re-send all active reminders, even recently sent ones.
+        #[clap(long, short)]
+        force: bool
+    },
+
+    /// Resumes a suspended task.
+    Resume {
+        words: Vec<String>,
+    },
+
+    /// Prints the active task stack.
+    Info,
+
+    /// Prints a list of all tasks, including suspended ones.
+    Ls,
+
+    /// Starts the notifier process, which sends wyd's reminder notifications.
+    Notifier {
+        // Kill active notifier processes without creating a new one.
+        #[clap(long, short)]
+        kill: bool,
+        #[clap(long = "become", short)]
+        #[clap(setting = ArgSettings::Hidden)]
+        become_id: Option<String>
+    }
+}
+
+#[derive(Clap, Debug)]
+struct Arguments {
+    #[clap(subcommand)]
+    subcommand: Option<Command>,
+}
+
+fn main() {
+    let args = Arguments::parse();
 
     let app_dir = dirs::data_local_dir()
         .expect("Could not locate current user's app data folder.")
@@ -156,33 +126,25 @@ since it re-triggers reminders that have already sent notifiactions recently.
     fs::create_dir_all(&app_dir).expect("Could not create application directory");
     let mut app = WydApplication::load(app_dir);
 
-    match matches.subcommand() {
-        ("push", Some(m)) => {
-            let label = word_args_to_string(m);
+    let subcommand = args.subcommand.unwrap_or(Command::Info);
+    use Command::*;
+    match subcommand {
+        Push { timebox, words } => {
+            let label = words.join(" ");
             if label.is_empty() {
                 eprintln!("Can't create a job without a label.");
                 return;
             }
-            let timebox = match m.value_of("timebox") {
-                Some(string) => {
-                    let dur = humantime::parse_duration(string).expect("Invalid timebox value.");
-                    Some(dur)
-                }
-                None => None,
-            };
-
             app.create_job(label, timebox);
         }
 
-        ("suspend", Some(m)) => {
-            let words = word_args_to_string(m);
+        Suspend { words, reason, timebox, new } => {
+            let words = words.join(" ");
             if words.is_empty() {
                 eprintln!("Can't perform suspend without a label.");
                 return;
             }
-            let reason = m.value_of("reason").unwrap_or("None").to_owned();
-            let timer = if let Some(timer_str) = m.value_of("timer") {
-                let std_duration = humantime::parse_duration(timer_str).expect("Invalid duration");
+            let timer = if let Some(std_duration) = timebox {
                 let utc_date = Utc::now()
                     + Duration::from_std(std_duration)
                         .expect("Unable to convert std duration to chrono duration.");
@@ -191,52 +153,43 @@ since it re-triggers reminders that have already sent notifiactions recently.
                 None
             };
 
-            if m.is_present("new") {
+            if new {
                 app.create_suspended_job(words, reason, timer);
             } else {
-                app.suspend_job_named(&words, reason, timer)
+                app.suspend_job_named(&words, reason, timer);
             }
             app.save();
         }
 
-        ("done", Some(_)) => {
+        Done => {
             app.complete_current_job();
         }
 
-        ("resume", Some(m)) => {
-            let pattern = word_args_to_string(m);
+        Resume { words } => {
+            let pattern = words.join(" ");
             app.resume_job_named(&pattern);
         }
 
-        ("notifier", Some(m)) => {
-            if m.is_present("kill") {
+        Notifier { kill, become_id } => {
+            if kill {
                 app.kill_notifier();
-            } else if let Some(id_str) = m.value_of("become") {
-                app.become_notifier(id_str);
+            } else if let Some(id_str) = become_id {
+                app.become_notifier(&id_str);
             } else {
                 app.spawn_notifier();
             }
         }
 
-        ("remind", Some(m)) => {
-            let force = m.is_present("force");
+        Remind { force } => {
             app.send_reminders(force);
         }
 
-        ("ls", Some(_)) => {
+        Ls => {
             app.ls_job_board();
         }
 
-        (missing, Some(_)) => {
-            unimplemented!("No implementation for subcommand {}", missing)
-        }
-
-        ("", None) => {
+        Info => {
             print!("{}", app.get_summary());
-        }
-
-        (invalid, None) => {
-            panic!("Invalid subcommand {}", invalid)
         }
     };
 }
